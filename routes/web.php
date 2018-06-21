a7bd9a101ff68178f6c2e3cfe7009068bddc3cfe
micou1830 <38948344+micou1830@users.noreply.github.com>
2018-06-21 09:38:34 +0200

    Add Route for all Guests Controller

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+/*
+ * Route vers la homepage
+ */
+Route::get('/', 'HomeController@home');
+
+/*
+ * Route vers la liste des posts
+ */
+Route::get('/posts', 'PostsController@index');
+
+/*
+ * Route vers un post
+ */
+Route::get('/posts/show', 'PostsController@show');
+
+/*
+ * Route vers la liste des catégories
+ */
+Route::get('/categories', 'CategoriesController@index');
+
+/*
+ * Route vers une catégorie de posts
+ */
+Route::get('/categories/show', 'CategoriesController@show');
